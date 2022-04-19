4782136f02f6807eb6a10b81fd75afa87bf7aa5e
AylinHuertaG <aylin.huertag@gmail.com>
2022-04-18 21:50:19 -0500

    Se corrige la prueba del ejemplo 5

--- weekly_mission_2/example5/pokemon.test.js
+++ weekly_mission_2/example5/pokemon.test.js
@@ -1,6 +1,8 @@
 import Pokemon from './pokemon'
 
-test('1) Create a new object pokemon', () => {
-  const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+describe('Unit Test for Pokemon', () => {
+  test('1) Create a new object pokemon', () => {
+    const myPokemon = new Pokemon('Pikachu')
+    expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+  });
 });
